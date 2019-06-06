d6379b1a7496b0b66a49f04853b26af462d94ad4
Azkael <azkaelgaleano@gmail.com>
2019-06-05 18:16:37 -0600

    REST server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,25 @@
-
-
 //=============================
 // PUERTO
 //=============================
 process.env.PORT = process.env.PORT || 3000;
+
+//=============================
+// ENTORNO
+//=============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============================
+// BASE DE DATOS
+//=============================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev')
+    urlDB = 'mongodb://localhost:27017/cafe'
+else
+    urlDB = 'mongodb+srv://azkael:SCgekfSCaS77mbxi@cluster0-yyfnx.mongodb.net/cafe';
+
+//mongodb://localhost:27017/cafe
+
+//mongodb://azkael:SCgekfSCaS77mbxi@cluster0-yyfnx.mongodb.net/cafe
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ app.use(bodyParser.json());
 
 app.use( require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', {
-    useFindAndModify: false,
+mongoose.connect(process.env.URLDB, {
     useCreateIndex: true,
     useNewUrlParser: true
 }, (err) => {
     if(err) console.log('No se pudo conectar a la base de datos!!!')
+    else console.log('Base de datos ONLINE!!')
 });
 
 app.listen(process.env.PORT, () => console.log(`Escuchando puerto ${process.env.PORT}`));
