52b81fe20d72855aae0eaf1c5b364858b4490e6a
Azkael <azkaelgaleano@gmail.com>
2019-06-10 01:08:09 -0600

    Change token expiration

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //60 minutos
 //24 horas
 //30 dias
-process.env.CADUCIDAD_TOKEN = '1h';
+process.env.CADUCIDAD_TOKEN = '48h';
 
 //=============================
 // SEED AUTH
